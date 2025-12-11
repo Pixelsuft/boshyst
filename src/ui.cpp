309698eb6c4743d166ef4fedd4e7eb395d71770f
Pixelsuft <Pixelsuft@gmail.com>
2025-12-11 10:35:25 +0700

    offsets

--- src/ui.cpp
+++ src/ui.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "ui.hpp"
 #include "conf.hpp"
 #include "mem.hpp"
@@ -30,6 +31,16 @@ void* get_player_ptr(int s) {
 		const size_t offsets[] = { 0x400000 + 0x59A9C, 0xA4, 0x7C0, 0xD0, 0x234, 0 };
 		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
 	}
+	// MB1
+	if (s == 6) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0xD8, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
+	// B2, MARIO SECRET
+	if (s == 7 || s == 33) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0xC8, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
 	// W3, B8
 	if (s == 8 || s == 26) {
 		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0x48, 0 };
@@ -46,6 +57,11 @@ void* get_player_ptr(int s) {
 		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0x88, 0 };
 		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
 	}
+	// B9
+	if (s == 59) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0x28, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
 	// W10
 	if (s == 31) {
 		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x234, 0x208, 0x1C, 0x68, 0 };
@@ -54,6 +70,51 @@ void* get_player_ptr(int s) {
 	// W11-cobrat
 	if (s == 35) {
 		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0x480, 0xC, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
+	// KAPPA
+	if (s == 37) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0x110, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
+	// FINAL PATH
+	if (s == 38) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0xB8, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
+	// PRIZE ROOM
+	if (s == 49) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0x140, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
+	// BLIZZARD
+	if (s == 53) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0xE8, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
+	// CHEETAH
+	if (s == 54) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0xE0, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
+	// GASTLY
+	if (s == 9) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0x130, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
+	// POKEWORLD
+	if (s == 50) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0x78, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
+	// METAL GEAR
+	if (s == 23) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0x418, 0 };
+		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+	}
+	// ELEVATOR
+	if (s == 51) {
+		const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, 0x138, 0 };
 		return mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
 	}
 	return nullptr;
@@ -68,8 +129,16 @@ void ui::draw() {
 	ImGui::SetNextWindowSize(ImVec2((float)conf::size[0], (float)conf::size[1]));
 	if (ImGui::Begin("Boshyst", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoSavedSettings)) {
 		// ImGui::Text("Boshyst by Pixelsuft");
+		static int last_scene = 0;
+		static int cur_frames = 0;
 		int scene_id = get_scene_id();
+		if (scene_id != last_scene) {
+			last_scene = scene_id;
+			cur_frames = 0;
+		}
+		cur_frames++;
 		uint8_t* pp = (uint8_t*)get_player_ptr(scene_id);
+		ImGui::Text("Cur Frames: %i", cur_frames);
 		if (pp != nullptr) {
 			static int last_x = 0;
 			static int last_y = 0;
@@ -85,6 +154,28 @@ void ui::draw() {
 			float ty = (float)(cur_y % 480) * (float)ws[1] / 480.f;
 			ImDrawList* draw_list = ImGui::GetBackgroundDrawList();
 			// draw_list->AddCircleFilled(ImVec2(tx, ty), 5.f, IM_COL32(255, 0, 0, 255), 12);
+		}
+		if (0) {
+			// Display all object IDs
+			ImDrawList* draw_list = ImGui::GetBackgroundDrawList();
+			const ImFont* font = ImGui::GetFont();
+			for (size_t i = 0; i < 2000; i += 8) {
+				const size_t offsets[] = { mem::get_base("Lacewing.mfx") + 0x2D680, 0x208, 0x1C, i, 0 };
+				pp = (uint8_t*)mem::ptr_from_offsets(offsets, sizeof(offsets) / 4);
+				if (!pp)
+					break;
+				int cur_x = *(int*)(pp + 0x4C);
+				int cur_y = *(int*)(pp + 0x54);
+				if (cur_x <= 0 || cur_x >= 4257664 || cur_y <= 0 || cur_y >= 4257664)
+					continue;
+				// cout << i << ": " << cur_x << ", " << cur_y << std::endl;
+				char buf[100];
+				// sprintf(buf, "%i, %i", cur_x, cur_y);
+				_itoa((int)i, buf, 10);
+				draw_list->AddRectFilled(ImVec2((float)(cur_x % 640), (float)(cur_y % 480)), ImVec2((float)(cur_x % 640 + 20), (float)(cur_y % 480 + 10)), IM_COL32(255, 255, 255, 255));
+				draw_list->AddText(font, ImGui::GetFontSize(), ImVec2((float)(cur_x % 640), (float)(cur_y % 480)), IM_COL32(255, 0, 0, 255), buf);
+				// draw_list->AddCircleFilled(ImVec2((float)(cur_x % 640), (float)(cur_y % 480)), 2.f, IM_COL32(255, 0, 0, 255), 4);
+			}
 		}
 		ImGui::Text("Scene ID: %i", scene_id);
 		ImGui::Text("Last new RNG value: %i", last_rng_val);
