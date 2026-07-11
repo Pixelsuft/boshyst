654c1067d378293f5082f80a3d13c12e9b018e7a
Pixelsuft <pixelsuft@gmail.com>
2026-07-11 20:32:30 +0700

    wip

--- src/audio.cpp
+++ src/audio.cpp
@@ -483,8 +483,8 @@ void on_audio_destroy() {
         bat.write_line("call audio_merge" + to_str(i) + ".bat");
     bat.write_line("ffmpeg -f concat -i audio_inputs.txt -c copy ../output.wav");
     bat.write_line("cd ..");
-    bat.write_line("pause");
     bat.write_line("echo Waiting to delete cache...");
+    bat.write_line("pause");
     bat.write_line("del temp_audio\\audio_inputs.txt");
     bat.write_line("del temp_audio\\audio_filters*.txt");
     bat.write_line("del temp_audio\\audio_merge*.bat");
